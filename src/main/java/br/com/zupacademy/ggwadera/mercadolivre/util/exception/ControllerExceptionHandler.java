845b568adb42771109a5d66e3aae2c517becc7f0
Guilherme Gwadera <16023489+ggwadera@users.noreply.github.com>
2021-04-26 18:48:36 -0300

    tratamento de exceções do controlador

---
+++ src/main/java/br/com/zupacademy/ggwadera/mercadolivre/util/exception/ControllerExceptionHandler.java
@@ -0,0 +1,35 @@
+package br.com.zupacademy.ggwadera.mercadolivre.util.exception;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+
+    private final MessageSource messageSource;
+
+    public ControllerExceptionHandler(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public List<ErrorResponse> handleFieldErrors(
+        MethodArgumentNotValidException exception
+    ) {
+        return exception.getBindingResult()
+            .getFieldErrors()
+            .stream()
+            .map(e -> new ErrorResponse(e.getField(), messageSource.getMessage(e, LocaleContextHolder.getLocale())))
+            .collect(Collectors.toList());
+    }
+
+}
---
+++ src/main/java/br/com/zupacademy/ggwadera/mercadolivre/util/exception/ErrorResponse.java
@@ -0,0 +1,20 @@
+package br.com.zupacademy.ggwadera.mercadolivre.util.exception;
+
+public class ErrorResponse {
+
+    private final String campo;
+    private final String erro;
+
+    public ErrorResponse(String campo, String erro) {
+        this.campo = campo;
+        this.erro = erro;
+    }
+
+    public String getCampo() {
+        return campo;
+    }
+
+    public String getErro() {
+        return erro;
+    }
+}
