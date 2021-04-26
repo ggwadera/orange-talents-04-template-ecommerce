e443d295807ba3fbca0a6b36d4b2419abe10d659
Guilherme Gwadera <16023489+ggwadera@users.noreply.github.com>
2021-04-26 16:15:59 -0300

    setup inicial

---
+++ src/test/java/br/com/zupacademy/ggwadera/mercadolivre/MercadoLivreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.ggwadera.mercadolivre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoLivreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
