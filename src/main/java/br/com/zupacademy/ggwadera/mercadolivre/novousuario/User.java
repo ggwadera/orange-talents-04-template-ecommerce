6e42a3f53fd567836f4ab19445aca814f4169fe1
Guilherme Gwadera <16023489+ggwadera@users.noreply.github.com>
2021-04-27 17:18:54 -0300

    adicionado implementação UserDetailsService

--- src/main/java/br/com/zupacademy/ggwadera/mercadolivre/novousuario/User.java
+++ src/main/java/br/com/zupacademy/ggwadera/mercadolivre/novousuario/User.java
@@ -1,6 +1,5 @@
 package br.com.zupacademy.ggwadera.mercadolivre.novousuario;
 
-import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.Generated;
 import org.hibernate.annotations.GenerationTime;
 import org.hibernate.annotations.NaturalId;
@@ -21,7 +20,7 @@ public class User {
 
     @NaturalId
     @Column(nullable = false)
-    private String login;
+    private String email;
 
     @Column(nullable = false)
     private String password;
@@ -34,11 +33,26 @@ public class User {
     public User() {
     }
 
-    public User(@NotBlank @Email String login, @NotNull PlaintextPassword password) {
-        Assert.hasLength(login, "login não pode estar em branco");
+    public User(@NotBlank @Email String email, @NotNull PlaintextPassword password) {
+        Assert.hasLength(email, "login não pode estar em branco");
         Assert.notNull(password, "senha não pode ser nula");
-        this.login = login;
+        this.email = email;
         this.password = password.hash();
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
 }
