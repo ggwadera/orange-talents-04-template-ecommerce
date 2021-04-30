84e68dafcb51ee321719ffeb202377d19a9b1f83
Guilherme Gwadera <16023489+ggwadera@users.noreply.github.com>
2021-04-30 12:03:19 -0300

    adicionando paremeter como target válido p/ validção ExistsById

--- src/main/java/br/com/zupacademy/ggwadera/mercadolivre/util/validation/ExistsById.java
+++ src/main/java/br/com/zupacademy/ggwadera/mercadolivre/util/validation/ExistsById.java
@@ -6,7 +6,7 @@ import java.lang.annotation.*;
 
 @Documented
 @Constraint(validatedBy = {ExistsByIdValidator.class})
-@Target({ElementType.FIELD})
+@Target({ElementType.FIELD, ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ExistsById {
   String message() default
