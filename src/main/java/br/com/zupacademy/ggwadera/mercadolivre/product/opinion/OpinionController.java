483f2945192bf5a9946bbc6a2811693d9908e9e1
Guilherme Gwadera <16023489+ggwadera@users.noreply.github.com>
2021-04-30 12:00:32 -0300

    refatorado controlador produto em controladores individuais

--- src/main/java/br/com/zupacademy/ggwadera/mercadolivre/product/opinion/OpinionController.java
+++ src/main/java/br/com/zupacademy/ggwadera/mercadolivre/product/opinion/OpinionController.java
@@ -1,12 +1,15 @@
-package br.com.zupacademy.ggwadera.mercadolivre.product;
+package br.com.zupacademy.ggwadera.mercadolivre.product.opinion;
 
+import br.com.zupacademy.ggwadera.mercadolivre.product.Product;
 import br.com.zupacademy.ggwadera.mercadolivre.security.AuthenticatedUser;
 import br.com.zupacademy.ggwadera.mercadolivre.user.User;
+import br.com.zupacademy.ggwadera.mercadolivre.util.validation.ExistsById;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -17,22 +20,22 @@ import javax.validation.Valid;
 @RequestMapping("/produtos")
 @Transactional
 @Validated
-public class ProductController {
+public class OpinionController {
 
   @PersistenceContext private EntityManager manager;
 
-  @InitBinder(value = "newProduct")
-  public void init(WebDataBinder binder) {
-    binder.addValidators(new DistinctFeatureNameValidator());
-  }
-
-  @PostMapping
-  public ResponseEntity<Void> newProduct(
-      @RequestBody @Valid NewProductRequest request,
+  @PostMapping("/{id}/opinioes")
+  public ResponseEntity<Void> addOpinion(
+      @PathVariable @ExistsById(domainClass = Product.class) Long id,
+      @RequestBody @Valid NewOpinionRequest request,
       @AuthenticationPrincipal AuthenticatedUser authenticatedUser) {
     User user = authenticatedUser.getUser();
-    Product product = request.toModel(manager, user);
-    manager.persist(product);
+    Product product = manager.find(Product.class, id);
+    if (product.belongsTo(user)) {
+      throw new ResponseStatusException(
+          HttpStatus.BAD_REQUEST, "Não pode opinar sobre o próprio produto.");
+    }
+    manager.persist(request.toModel(user, product));
     return ResponseEntity.ok().build();
   }
 }
